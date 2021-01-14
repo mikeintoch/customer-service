f4a53e67f0e7fa483d5771ad1a7fe00fbfd7cb91
Miguel Delgadillo <mdelgadi@redhat.com>
2021-01-13 23:45:49 -0600

    First Commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+// Add Customer data
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00001', 'Pedro', 'Perez', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00002', 'Miguel', 'Rodriguez', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00003', 'Jorge', 'Fernandez', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00004', 'Ramon', 'Ruiz', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00005', 'Isaac', 'Lopez', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00006', 'Alonso', 'De la Peña', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00007', 'Adrian', 'Cataño', 5236.36);
+INSERT INTO CUSTOMER (id,accountId, name, lastname, balance) values (nextval('hibernate_sequence'),'C00008', 'Javier', 'Dominguez', 5236.36);
